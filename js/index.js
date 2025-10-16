f887181ebc22396bc783837c080d67ae1e79c7db
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-16 17:49:07 +0530

    new contents added

---
+++ js/index.js
@@ -0,0 +1,50 @@
+
+// function myFunction(){
+
+//     console.log("aliyooooooo......");       //maximum call stack size executed
+//     myFunction()
+    
+// }
+// myFunction()
+
+
+
+// let x=1
+// function myFunction(){
+
+//     console.log("aliyooooooo......");        //aliyoooooo.....  aliyoooooo.....  aliyoooooo.....  aliyoooooo.....  aliyoooooo.....
+//     if(x<5)
+//     {
+//         x++
+//         myFunction()
+//     }
+    
+// }
+// myFunction()
+
+
+
+// function aliya(x){
+
+   
+//     if(x<=5){
+
+//         console.log("aliyooo......."+x);
+//         aliya( x++)
+        
+//     }
+    
+// }
+// aliya(5)
+
+
+
+// function asd(n){
+
+//     if(n==0){
+
+//         return 1;                //120
+//     }
+//     return n * asd(n-1);
+// }
+// console.log(asd(5));
